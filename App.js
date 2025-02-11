b2a4651920951d0b908983343d613647fd74ad16
mnofrizal <mnofrizal1996@gmail.com>
2025-02-11 19:12:23 +0700

    ok

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-bold">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl font-bold">
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
